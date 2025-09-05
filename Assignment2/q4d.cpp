4f9c26ca10abb63d38588e27b2345a843146ba87
SakshamNagrath29292 <snagrath_be24@thapar.edu>
2025-09-06 01:36:04 +0530

    Update q4d.cpp

--- Assignment2/q4d.cpp
+++ Assignment2/q4d.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
-int main()
-{
+
+int main() {
     int n;
     cout << "Enter number of strings: ";
     cin >> n;
-    char arr[100][100], temp[100];
-    for (int i = 0; i < n; i++)
+
+    string arr[100];
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (strcmp(arr[i], arr[j]) > 0)
-            {
-                strcpy(temp, arr[i]);
-                strcpy(arr[i], arr[j]);
-                strcpy(arr[j], temp);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                string temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    for (int i = 0; i < n; i++)
+
+    cout << "Sorted strings: ";
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
+
