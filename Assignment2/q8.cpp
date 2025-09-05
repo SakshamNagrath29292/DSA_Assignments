8e625595a70af45cdd0aeb2b04d974b6da655859
SakshamNagrath29292 <snagrath_be24@thapar.edu>
2025-09-06 01:52:02 +0530

    Update q8.cpp

--- Assignment2/q8.cpp
+++ Assignment2/q8.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     int n;
+    cout << "Enter size of array: ";
     cin >> n;
+
     int arr[100];
-    for (int i = 0; i < n; i++)
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-    int count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int j;
-        for (j = 0; j < i; j++)
-        {
-            if (arr[i] == arr[j])
-                break;
-        }
-        if (i == j)
-            count++;
     }
-    cout << count;
+
+    int distinctCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        bool isDuplicate = false;
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            distinctCount++;
+        }
+    }
+
+    cout << "Total distinct elements: " << distinctCount << endl;
+
+    return 0;
 }
