a06edc72ac0eed1735c758b531ae034bcf991ce2
SakshamNagrath29292 <snagrath_be24@thapar.edu>
2025-09-06 00:52:26 +0530

    104a.cpp

--- Assignment1/104.cpp
+++ Assignment1/104.cpp
@@ -27,38 +27,6 @@ void Reverse_array(int arr[], int size)
         arr[size - 1 - i] = temp;
     }
 }
-void multiplyMatrices(int a[][10], int b[][10], int result[][10], int r1, int c1, int r2, int c2)
-{
-    if (c1 != r2)
-    {
-        return;
-        cout << "Matrix muliplication not possible (c1 != r2)\n";
-    }
-    for (int i = 0; i < r1; i++)
-    {
-        for (int j = 0; j < c2; j++)
-        {
-            result[i][j] = 0;
-        }
-    }
-    for (int i = 0; i < r1; i++)
-    {
-        for (int j = 0; j < c2; j++)
-        {
-            for (int k = 0; k < c1; k++)
-            {
-                result[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-}
-void transpose(int arr[][10],int brr[][10],int col,int row){
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-            arr[j][i]=brr[i][j];
-        }
-    }
-}
 int main()
 {
     int n;
