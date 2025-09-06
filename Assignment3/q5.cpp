ba7f7753ea5365d6f2f1317b94a8c141e51544a9
SakshamNagrath29292 <snagrath_be24@thapar.edu>
2025-09-06 23:21:39 +0530

    Update q5.cpp

--- Assignment3/q5.cpp
+++ Assignment3/q5.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
-#include <cstring>
+#include <stack>
+#include <string>
+#include <cctype>
 using namespace std;
-#define MAX 100
-int stack[MAX];int top=-1;
-void push(int x){stack[++top]=x;}
-int pop(){return stack[top--];}
-int main(){
-    char exp[100];cin>>exp;int n=strlen(exp);
-    for(int i=0;i<n;i++){
-        char c=exp[i];
-        if(isdigit(c))push(c-'0');
-        else{
-            int b=pop(),a=pop();
-            if(c=='+')push(a+b);
-            else if(c=='-')push(a-b);
-            else if(c=='*')push(a*b);
-            else if(c=='/')push(a/b);
+
+int evaluatePostfix(string postfix) {
+    stack<int> s;
+
+    for (char ch : postfix) {
+        if (isdigit(ch)) {
+            s.push(ch - '0');
+        }
+        else {
+            int val2 = s.top(); s.pop();
+            int val1 = s.top(); s.pop();
+
+            switch (ch) {
+                case '+': s.push(val1 + val2); break;
+                case '-': s.push(val1 - val2); break;
+                case '*': s.push(val1 * val2); break;
+                case '/': s.push(val1 / val2); break;
+            }
         }
     }
-    cout<<pop();
+
+    return s.top();
 }
+
+int main() {
+    string postfix;
+    cout << "Enter a postfix expression (single-digit operands): ";
+    cin >> postfix;
+
+    cout << "Result = " << evaluatePostfix(postfix) << endl;
+    return 0;
+}
+
